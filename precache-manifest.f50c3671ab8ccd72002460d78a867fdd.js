f73af5b88d05cef606c2c99bd80463072f37fa60
Joanna Jankowska <1081578@ucn.dk>
2020-04-21 19:01:28 +0200

    Updates

---
+++ precache-manifest.f50c3671ab8ccd72002460d78a867fdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ca25806ffbc11734989049b5d88a92",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "e44c27ee94312f8f3975",
+    "url": "/react-recipe-app/static/css/main.764c6c06.chunk.css"
+  },
+  {
+    "revision": "20b5a8ddb239b4c826c0",
+    "url": "/react-recipe-app/static/js/2.9a629af1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-recipe-app/static/js/2.9a629af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44c27ee94312f8f3975",
+    "url": "/react-recipe-app/static/js/main.ae869435.chunk.js"
+  },
+  {
+    "revision": "90ea6a60b7852282587a",
+    "url": "/react-recipe-app/static/js/runtime-main.111060c6.js"
+  }
+]);
